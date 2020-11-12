a11b4a66cf1a2e373e7870e77850c91a5d5be922
SunnyRain224 <71274750+SunnyRain224@users.noreply.github.com>
2020-11-12 17:56:23 +0200

    Add files via upload

---
+++ SecondExercise.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.util.Scanner;
+
+public class SecondExercise {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите количество чисел : ");
+        int k;//Количество чисел
+        int avg = 0;//Среднее арифметическое
+        if (in.hasNextInt()) {
+            k = in.nextInt();
+        } else {
+            System.out.println("Error");
+            return;
+        }
+        System.out.println("Количество чисел : " + k);
+        int[] arr = new int[k];
+        System.out.println("Введите значения чисел : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+            avg += arr[i];
+        }
+        System.out.println("Вы ввели : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Среднее арифметическое введенных чисел = " + ((double) avg / arr.length));
+
+    }
+}
